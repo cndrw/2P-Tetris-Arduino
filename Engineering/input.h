96b83cce71e6ce3fd517ac75d6dba3bf50e1acdb
cndrw <cowalther2@gmail.com>
2024-02-27 13:52:40 +0100

    retris version 1
    and some changes to the input files

--- Engineering/input.h
+++ Engineering/input.h
@@ -1,17 +1,13 @@
-// int DATA_CLOCK    = 6;
-// int DATA_LATCH    = 7;
-// int DATA_CLOCK_2    = 2;
-// int DATA_LATCH_2  	= 3;
-// int DATA_SERIAL_1  = 12;
-// int DATA_SERIAL_2  = 4;
-
 #ifndef INPUT_H
 #define INPUT_H
 
 #define CONTROLLER_1 0
 #define CONTROLLER_2 1
-#define BUTTON_PRESSED 0
 
+#define BUTTON_PRESSED 0
+#define BUTTON_NOT_PRESSED 1
+
+#define BUTTON_AMOUNT 12
 #define BUTTON_B 0
 #define BUTTON_Y 1
 #define BUTTON_SELECT 2
@@ -25,211 +21,106 @@
 #define BUTTON_L 10
 #define BUTTON_R 11
 
+namespace Input {
 
-
-// int buttons_1[12];
-// int buttons_2[12];
-
-namespace Input 
-{ // {left controller port, right controller port}
-  static constexpr uint8_t dataLatch[] = { 3, 7 };
-  static constexpr uint8_t dataClock[] = { 2, 6 };
-  static constexpr uint8_t dataSerial[] = { 4, 12 };
-  static bool buttons[2][12] = {1};
-
-    
-    void SetupPins()
-    {
-         // Set DATA_CLOCK normally HIGH
-         for (int i = 0; i < 2; i++)
-         {
-  pinMode(dataClock[i], OUTPUT);
-  digitalWrite(dataClock[i], HIGH);
-
-  pinMode(dataLatch[i], OUTPUT);
-  digitalWrite(dataLatch[i], LOW);
-  pinMode(dataSerial[i], OUTPUT);
-  digitalWrite(dataSerial[i], HIGH);
-  pinMode(dataSerial[i], INPUT);  
-         }
-
-  // pinMode(DATA_CLOCK_2, OUTPUT);
-  // digitalWrite(DATA_CLOCK_2, HIGH);
-  
-  // Set DATA_LATCH normally LOW
-
-  // pinMode(DATA_LATCH_2, OUTPUT);
-  // digitalWrite(DATA_LATCH_2, LOW);
-
-  // Set DATA_SERIAL normally HIGH
-
-  // pinMode(DATA_SERIAL_2, OUTPUT);
-  // digitalWrite(DATA_SERIAL_2, HIGH);
-  // pinMode(DATA_SERIAL_2, INPUT);  
-    }
-
-
-    void ReadControllerInputs()
-    {
-    // Latch for 12us
-    for (uint8_t j = 0; j < 2; j++)
-    {
-
-      digitalWrite(dataLatch[j], HIGH);
-      delayMicroseconds(12);
-      digitalWrite(dataLatch[j], LOW);
-      delayMicroseconds(6);
-    
-      // Retrieve button presses from shift register by pulling the clock high for 6us
-      for(uint8_t i = 0; i < 16; i++)
-      {
-          digitalWrite(dataClock[j], LOW);
-          delayMicroseconds(6);
-          if(i <= 16)
-          {
-              buttons[j][i] = digitalRead(dataSerial[j]);
-              // Serial.println(buttons[j][i]);
-          }
-          digitalWrite(dataClock[j], HIGH);
-          delayMicroseconds(6);
-      }   
-    }
-    }
-    // controller is 0 or 1 depending on which controller is being read; key is the button being checked
-    bool GetKey(const uint8_t controller, const uint8_t key)
-    {
-      return buttons[controller][key] == BUTTON_PRESSED;
-    }
-
-    // static Controller m_controllers[2];
-    // uint8_t controllerCount = 0;
+struct Controller
+{
+  bool currInput[BUTTON_AMOUNT] = {1};
+  bool lastInput[BUTTON_AMOUNT] = {1};
 };
 
+static constexpr uint8_t dataLatch[] = { 3, 7 };
+static constexpr uint8_t dataClock[] = { 2, 6 };
+static constexpr uint8_t dataSerial[] = { 4, 12 };
+// static bool buttons[2][12] = {1}; 
+static Controller controllers[2]; // max 2 controller with 12 buttons
+static uint8_t controllerCount = 2;
+
+
+/**
+ * @brief sets how many controller are actively checked
+ * @param number number of controller
+*/
+void SetControllerCount(const uint8_t number)
+{
+  controllerCount = number;
+}
+    
+/**
+ * @brief setups the controller pins
+*/
+void SetupPins()
+{
+  for (int i = 0; i < controllerCount; i++)
+  {
+    // Set DATA_CLOCK normally HIGH
+    pinMode(dataClock[i], OUTPUT);
+    digitalWrite(dataClock[i], HIGH);
+
+    pinMode(dataLatch[i], OUTPUT);
+    digitalWrite(dataLatch[i], LOW);
+
+    pinMode(dataSerial[i], OUTPUT);
+    digitalWrite(dataSerial[i], HIGH);
+    pinMode(dataSerial[i], INPUT);  
+  }
+}
+
+/**
+ * @brief reads the current state of the controller(s)
+*/
+void ReadControllerInputs()
+{
+  // Latch for 12us
+  for (uint8_t j = 0; j < controllerCount; j++)
+  {
+
+    digitalWrite(dataLatch[j], HIGH);
+    delayMicroseconds(12);
+    digitalWrite(dataLatch[j], LOW);
+    delayMicroseconds(6);
+
+    // Retrieve button presses from shift register by pulling the clock high for 6us
+    for(uint8_t i = 0; i < 16; i++)
+    {
+      digitalWrite(dataClock[j], LOW);
+      delayMicroseconds(6);
+      if(i <= 16)
+      {
+        // current inputs will be written in last input befor getting updated
+        memcpy(controllers[j].lastInput, controllers[j].currInput, sizeof(bool) * BUTTON_AMOUNT);
+        controllers[j].currInput[i] = digitalRead(dataSerial[j]);
+      }
+      digitalWrite(dataClock[j], HIGH);
+      delayMicroseconds(6);
+    }   
+  }
+}
+
+/**
+ * @brief checks if the desired button is currently pressed
+ * @param controller which controller shall be checked
+ * @param button which button shall be checked
+*/
+bool GetButton(const uint8_t controller, const uint8_t button)
+{
+  // if the button is pressed it is represented with a 0 in the buttons array
+  return controllers[controller].currInput[button] == BUTTON_PRESSED;
+}
+
+
+/**
+ * @brief checks if the desired button got pressed (only the transition)
+ * @param controller which controller shall be checked
+ * @param button which button shall be checked
+*/
+bool GetButtonDown(const uint8_t controller, const uint8_t button)
+{
+  // checked if there is a falling signal => button got pressed
+  return (controllers[controller].currInput[button] == BUTTON_PRESSED) &&
+         (controllers[controller].lastInput[button] == BUTTON_NOT_PRESSED);
+}
+
+}; // Input
 
 #endif
-
-// void setupPins(void){
- 
-// }
-
-
-// Prints which buttons are pressed
-// void printButtons_1(void){
-//     if(buttons_1[0] == 0){
-//         Serial.print("B");
-//     }
-//     if(buttons_1[1] == 0){
-//         Serial.print("Y");
-//     }
-//     if(buttons_1[2] == 0){
-//         Serial.print("SELECT");
-//     }
-//     if(buttons_1[3] == 0){
-//         Serial.print("START");
-//     }
-//     if(buttons_1[4] == 0){
-//         Serial.print("UP");
-//     }
-//     if(buttons_1[5] == 0){
-//         Serial.print("DOWN");
-//     }
-//     if(buttons_1[6] == 0){
-//         Serial.print("LEFT");
-//     }
-//     if(buttons_1[7] == 0){
-//         Serial.print("RIGHT");
-//     }
-//     if(buttons_1[8] == 0){
-//         Serial.print("A");
-//     }
-//     if(buttons_1[9] == 0){
-//         Serial.print("X");
-//     }
-//     if(buttons_1[10] == 0){
-//         Serial.print("LB");
-//     }
-//     if(buttons_1[11] == 0){
-//         Serial.print("RB");
-//     }
-// }
-
-// void printButtons_2(void){
-//     if(buttons_2[0] == 0){
-//         Serial.print("B");
-//     }
-//     if(buttons_2[1] == 0){
-//         Serial.print("Y");
-//     }
-//     if(buttons_2[2] == 0){
-//         Serial.print("SELECT");
-//     }
-//     if(buttons_2[3] == 0){
-//         Serial.print("START");
-//     }
-//     if(buttons_2[4] == 0){
-//         Serial.print("UP");
-//     }
-//     if(buttons_2[5] == 0){
-//         Serial.print("DOWN");
-//     }
-//     if(buttons_2[6] == 0){
-//         Serial.print("LEFT");
-//     }
-//     if(buttons_2[7] == 0){
-//         Serial.print("RIGHT");
-//     }
-//     if(buttons_2[8] == 0){
-//         Serial.print("A");
-//     }
-//     if(buttons_2[9] == 0){
-//         Serial.print("X");
-//     }
-//     if(buttons_2[10] == 0){
-//         Serial.print("LB");
-//     }
-//     if(buttons_2[11] == 0){
-//         Serial.print("RB");
-//     }
-// }
-
-// void getControllerData_1(void){
-//     // Latch for 12us
-//     digitalWrite(DATA_LATCH, HIGH);
-//     delayMicroseconds(12);
-//     digitalWrite(DATA_LATCH, LOW);
-//     delayMicroseconds(6);
-    
-//     // Retrieve button presses from shift register by pulling the clock high for 6us
-//     for(int i = 0; i < 16; i++){
-//         digitalWrite(DATA_CLOCK, LOW);
-//         delayMicroseconds(6);
-//         if(i <= 16){
-//             buttons_1[i] = digitalRead(DATA_SERIAL_1);
-//         }
-//         digitalWrite(DATA_CLOCK, HIGH);
-//             delayMicroseconds(6);
-
-//     }
-//   printButtons_1();  
-// }
-
-// void getControllerData_2(void){
-//     // Latch for 12us
-//     digitalWrite(DATA_LATCH_2, HIGH);
-//     delayMicroseconds(12);
-//     digitalWrite(DATA_LATCH_2, LOW);
-//     delayMicroseconds(6);
-    
-//     // Retrieve button presses from shift register by pulling the clock high for 6us
-//     for(int i = 0; i < 16; i++){
-//         digitalWrite(DATA_CLOCK_2, LOW);
-//         delayMicroseconds(6);
-//         if(i <= 11){
-//             buttons_2[i] = digitalRead(DATA_SERIAL_2);
-//         }
-//         digitalWrite(DATA_CLOCK_2, HIGH);
-//             delayMicroseconds(6);
-
-//     }
-//   printButtons_2();  
-// }
